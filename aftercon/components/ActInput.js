357325c41642f25b9db333a7a5982c59d4740984
omarfrt <iroonix5@gmail.com>
2022-08-19 15:40:58 +0100

    usestate done for now

--- aftercon/components/ActInput.js
+++ aftercon/components/ActInput.js
@@ -1,12 +1,26 @@
-import Styles from "../styles/hero.module.css"
+import { useState } from "react";
+import Styles from "../styles/hero.module.css";
 
-const ActInput = ()=>{
-    return (
-        <div className={Styles.action_input_container}>
-            <input placeholder="@"></input>
-            <button>Get Early Access</button>
-          </div>
-    )
-}
+const ActInput = () => {
+  const [contactInfo, SetContacInfo] = useState("");
+  const handleChange = (event) => {
+    SetContacInfo(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    alert(contactInfo);
+  };
+  return (
+    <div className={Styles.action_input_container}>
+      <input
+        placeholder="@"
+        name="email"
+        type="email"
+        onChange={handleChange}
+        value={contactInfo}
+      ></input>
+      <button onClick={handleSubmit}>Get Early Access</button>
+    </div>
+  );
+};
 
 export default ActInput;
